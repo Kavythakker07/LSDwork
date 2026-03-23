c2f0630f7ce153c03f188c8ebd73480eafd569e5
kavyThakar <160318745+Kavythakker07@users.noreply.github.com>
2026-03-23 15:47:14 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,23 +4,27 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async (req, file) => {
-    let folder = "uploads";
+ params: async (req, file) => {
+  let folder = "uploads";
 
-    if (req.originalUrl.includes("uploadVideo")) {
-      folder = "videos";
-    } else if (req.originalUrl.includes("updateProfile")) {
-      folder = "avatars";
-    }
+  if (req.originalUrl.includes("uploadVideo")) {
+    folder = "videos";
+  } else if (req.originalUrl.includes("updateProfile")) {
+    folder = "avatars";
+  } else if (req.originalUrl.includes("addCourses")) {
+    folder = "thumbnails";
+  }
 
-    return {
-      folder,
-      resource_type: "auto", // important for videos
-      public_id: Date.now() + "-" + file.originalname,
-    };
-  },
+  return {
+    folder,
+    resource_type: "auto",
+    public_id: Date.now() + "-" + file.originalname.split('.')[0], // 🔥 FIX DOUBLE EXT
+  };
+}
 });
 
 const upload = multer({ storage });
 
 module.exports = upload;
+
+
